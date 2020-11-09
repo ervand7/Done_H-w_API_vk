71c2cc1a87986b937680d350755a3a713c156415
ervand7 <ervand7@mail.ru>
2020-11-09 13:37:25 +0300

    correction

--- vk task_1.py
+++ vk task_1.py
@@ -2,45 +2,45 @@ import requests
 from urllib.parse import urlencode, urljoin
 from pprint import pprint
 # ____________________ 1-st stage: we get token ____________________________________________________
-#
-# oauth_api_base_url = 'https://oauth.vk.com/authorize'
-# APP_ID = 7649081
-# redirect_uri = 'https://oauth.vk.com/blank.html'
-# scope = 'friends'
-#
-# oauth_params = {
-#     'redirect_uri': redirect_uri,
-#     'scope': scope,
-#     'response_type': 'token',
-#     'client_id': APP_ID
-# }
-#
-# print('?'.join([oauth_api_base_url, urlencode(oauth_params)]))
+
+oauth_api_base_url = 'https://oauth.vk.com/authorize'
+APP_ID = 7649081
+redirect_uri = 'https://oauth.vk.com/blank.html'
+scope = 'friends'
+
+oauth_params = {
+    'redirect_uri': redirect_uri,
+    'scope': scope,
+    'response_type': 'token',
+    'client_id': APP_ID
+}
+
+print('?'.join([oauth_api_base_url, urlencode(oauth_params)]))
 
 # ____________________  2-nd stage: we already have the token  ____________________________________________________
-TOKEN = ''
-API_BASE_URL = 'https://api.vk.com/method/'
-V = '5.21'
-
-class VKUser:
-    def __init__(self, token=TOKEN, version=V):
-        self.token = token
-        self.version = version
-
-    def get_common_friends(self):
-        status_common_lst = urljoin(API_BASE_URL, 'friends.getMutual')
-        print(status_common_lst)
-        response = requests.get(status_common_lst, params={
-            'access_token': self.token,
-            'v': self.version,
-            'target_uid': 280572200,
-            'source_uid': 435521107
-        })
-        n = sorted(response.json()['response'])
-        for i in n:
-            pprint(i)
-
-a = VKUser()
-a.get_common_friends()
+# TOKEN = ''
+# API_BASE_URL = 'https://api.vk.com/method/'
+# V = '5.21'
+#
+# class VKUser:
+#     def __init__(self, token=TOKEN, version=V):
+#         self.token = token
+#         self.version = version
+#
+#     def get_common_friends(self):
+#         status_common_lst = urljoin(API_BASE_URL, 'friends.getMutual')
+#         print(status_common_lst)
+#         response = requests.get(status_common_lst, params={
+#             'access_token': self.token,
+#             'v': self.version,
+#             'target_uid': 280572200,
+#             'source_uid': 435521107
+#         })
+#         n = sorted(response.json()['response'])
+#         for i in n:
+#             pprint(i)
+#
+# a = VKUser()
+# a.get_common_friends()
 
 
--- vk task_2&3.py
+++ vk task_2&3.py
@@ -20,21 +20,23 @@ import tqdm
 # print('?'.join([oauth_api_base_url, urlencode(oauth_params)]))
 
 # ____________________  2-nd stage: we already have the token  ____________________________________________________
-TOKEN = ''
+TOKEN = 'c000969e3c270b9947bd26aea03beb35d31797026865f9f46da7459e45fb815b6d3a0e1644019c45ac6c9'
 API_BASE_URL = 'https://api.vk.com/method/'
 V = '5.21'
 
 input_of_ids = input(
-    'Введите через пробел два id, которые вы хотите проанализировать на получение общих друзей. '
+    'Введите через пробел два id, которые вы хотите проанализировать на \nполучение общих друзей. '
     'Затем нажмите Enter: ').split(' ')
-two_ids = list(map(int, input_of_ids))  # use for example: 280572200 435521107
+input_of_ids_items = list(map(int, input_of_ids))  # use for example: 280572200 435521107
 
 
-class VKUser_1:
-    def __init__(self, token=TOKEN, version=V, id=two_ids[0]):
+# __________________________________________________________________________________________
+# My main logic
+class VKUser:
+    def __init__(self, user_id=0, token=TOKEN, version=V):
         self.token = token
         self.version = version
-        self.id = id
+        self.id = user_id
 
     def count_of_all_friends(self):
         sheet_on_demand_vk = urljoin(API_BASE_URL, 'friends.search')
@@ -47,12 +49,12 @@ class VKUser_1:
         return s
 
     def get_full_list_friends(self):
-        status_common_lst = urljoin(API_BASE_URL, 'friends.search')
-        response = requests.get(status_common_lst, params={
+        data_demand_of_vk = urljoin(API_BASE_URL, 'friends.search')
+        response = requests.get(data_demand_of_vk, params={
             'access_token': self.token,
             'v': self.version,
             'user_id': self.id,
-            'count': a.count_of_all_friends()
+            'count': self.count_of_all_friends()
         })
         # check accurate count of friends
         # counter = 0
@@ -60,65 +62,32 @@ class VKUser_1:
         #     counter +=1
         # print(counter)
 
-        set_1 = set()
+        set_ = set()
         for i in response.json()['response']['items']:
-            set_1.add(i['id'])
-        return set_1
+            set_.add(i['id'])
+        return set_
 
 
-a = VKUser_1()
+man_1 = VKUser(input_of_ids_items[0])
+man_2 = VKUser(input_of_ids_items[1])
 
 
-class VKUser_2:
-    def __init__(self, token=TOKEN, version=V, id=two_ids[1]):
-        self.token = token
-        self.version = version
-        self.id = id
-
-    def count_of_all_friends(self):
-        sheet_on_demand_vk = urljoin(API_BASE_URL, 'friends.search')
-        response = requests.get(sheet_on_demand_vk, params={
-            'access_token': self.token,
-            'v': self.version,
-            'user_id': self.id
-        })
-        s = int(response.json()['response']['count'])
-        return s
-
-    def get_full_list_friends(self):
-        status_common_lst = urljoin(API_BASE_URL, 'friends.search')
-        response = requests.get(status_common_lst, params={
-            'access_token': self.token,
-            'v': self.version,
-            'user_id': self.id,
-            'count': b.count_of_all_friends()
-        })
-        # check accurate count of friends
-        # counter = 0
-        # for i in response.json()['response']['items']:
-        #     counter +=1
-        # print(counter)
-
-        set_2 = set()
-        for i in response.json()['response']['items']:
-            set_2.add(i['id'])
-        return set_2
-
-
-b = VKUser_2()
-
-
-def finder_common_friends(first_user, second_user):
-    common_friends_id = list(first_user.get_full_list_friends() & second_user.get_full_list_friends())
-    for i in tqdm.tqdm(sorted(common_friends_id)):
+def crossing(main_user=man_1, other_user=man_2):
+    mutal_user_list = (main_user.get_full_list_friends()) & (other_user.get_full_list_friends())
+    for i in tqdm.tqdm(sorted(mutal_user_list)):
         pprint(i)
-    print(f'Это был список с id общих друзей в ВК между аккаунтами с id {two_ids[0]} и {two_ids[1]}.')
 
 
-finder_common_friends(a, b)
+crossing()
 
+print(f'\nВыше был представлен список бщих друзей между пользователями \n'
+      f'с id {input_of_ids_items[0]} и {input_of_ids_items[1]}.')
+# __________________________________________________________________________________________
+# url getting
+
+print('\nА это ссылки на их профили:')
 begin_url = 'https://vk.com/'
-user_1_url = urljoin(begin_url, f"id{str(two_ids[0])}")
-user_2_url = urljoin(begin_url, f"id{str(two_ids[1])}")
-print(user_1_url)
-print(user_2_url)
+man_1_url = urljoin(begin_url, f"id{str(input_of_ids_items[0])}")
+man_2_url = urljoin(begin_url, f"id{str(input_of_ids_items[1])}")
+print(man_1_url)
+print(man_2_url)
